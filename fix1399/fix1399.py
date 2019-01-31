7874adfd4fb3a11a692c4e50a00589a39a94efab
Humorous Baby <44451911+HumorBaby@users.noreply.github.com>
2019-01-31 16:59:11 -0500

    Layout module

---
+++ fix1399/fix1399.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+
+from __future__ import unicode_literals, absolute_import, division, print_function
+
+from sopel import module
+
+def configure(config):
+  pass
+
+def setup(bot):
+  pass
+
